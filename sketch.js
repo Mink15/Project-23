f6198f623e7032097dc4ae47ddfa327a6d6ed9c0
swatimukut <68349619+swatimukut@users.noreply.github.com>
2021-05-03 18:33:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -77,14 +77,31 @@ function draw() {
  
  
   drawSprites();
-   parcel.Display();
+
 
 
 }
 
 function keyPressed(){
-	if(keyCode === DOWN_ARROW){
- 	  Matter.body.setStatic(packageBody,false);
+	
+function keyPressed(){ 
+if (keyCode === LEFT_ARROW) {
 
-	}
+    helicopterSprite.x=helicopterSprite.x-20;    
+    translation={x:-20,y:0}
+    Matter.Body.translate(packageBody, translation)
+
+
+  } 
+
+else if (keyCode === RIGHT_ARROW) {
+    helicopterSprite.x=helicopterSprite.x+20;
+    translation={x:20,y:0}
+    Matter.Body.translate(packageBody, translation)
+  }
+
+  else if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    
+  }
 }
